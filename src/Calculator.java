a80e92bbe40337b0280ff2953c3eee8c848e7251
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-06-30 21:21:10 +0200

    Calculator -> mult and div added.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,17 @@
 public class Calculator {
-    public int sumNumbers(int a, int b){
+    public int sumNumbers(int a, int b) {
         return a + b;
     }
 
     public int subNumbers(int a, int b) {
         return a - b;
+    }
+
+    public int multNumbers(int a, int b) {
+        return a * b;
+    }
+
+    public int divNumbers(int a, int b) {
+        return (b != 0) ? a / b : 0;
     }
 }
--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -18,4 +18,18 @@ public class CalculatorTest {
         int result = calc.subNumbers(10, 5);
         assertEquals(5 , result);
     }
+
+    @Test
+    public void mult() {
+        Calculator calc = new Calculator();
+        int result = calc.multNumbers(7, 3);
+        assertEquals(21 , result);
+    }
+
+    @Test
+    public void div() {
+        Calculator calc = new Calculator();
+        int result = calc.divNumbers(24, 2);
+        assertEquals(12 , result);
+    }
 }
