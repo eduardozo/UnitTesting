07a41183fb020a4e0f6b630ee0fe59c037c12d51
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-06-30 21:44:39 +0200

    CalculatorTest -> @before.

--- tests/calculator/CalculatorTest.java
+++ tests/calculator/CalculatorTest.java
@@ -1,35 +1,38 @@
 package calculator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+    Calculator calc;
+
+    @Before
+    public void before() {
+        calc = new Calculator();
+    }
 
     @Test
-    public void sum() {
-        Calculator calc = new Calculator();
+    public void testSum() {
         int result = calc.sumNumbers(3, 7);
         assertEquals(10, result);
     }
 
     @Test
-    public void sub() {
-        Calculator calc = new Calculator();
+    public void testSub() {
         int result = calc.subNumbers(10, 5);
         assertEquals(5 , result);
     }
 
     @Test
-    public void mult() {
-        Calculator calc = new Calculator();
+    public void testMult() {
         int result = calc.multNumbers(7, 3);
         assertEquals(21 , result);
     }
 
     @Test
-    public void div() {
-        Calculator calc = new Calculator();
+    public void testDiv() {
         int result = calc.divNumbers(24, 2);
         assertEquals(12 , result);
     }
