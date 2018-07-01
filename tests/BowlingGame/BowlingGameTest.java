0f13b8b4dfd90d7b6ea401c0fa5aa65afea12939
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-01 02:00:17 +0200

    TDD -> BowlingGame refactoring and test unit finished.

--- tests/BowlingGame/BowlingGameTest.java
+++ tests/BowlingGame/BowlingGameTest.java
@@ -1,27 +1,65 @@
 package BowlingGame;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BowlingGameTest {
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() throws Exception {
+        bowlingGame = new BowlingGame();
+    }
 
     @Test
     public void testGutterGame() {
-        BowlingGame bowlingGame = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            bowlingGame.roll(0);
-        }
-        assertEquals(0,bowlingGame.score());
+        rollMany(20, 0);
+        assertEquals(0, bowlingGame.score());
     }
 
     @Test
     public void testAllOnes() {
-        BowlingGame bowlingGame = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            bowlingGame.roll(1);
-        }
+        rollMany(20, 1);
         assertEquals(20, bowlingGame.score());
     }
 
+    @Test
+    public void testOneSpare() {
+        rollSpare();
+        bowlingGame.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, bowlingGame.score());
+    }
+
+    @Test
+    public void testOneStrike() {
+        rollStrike();
+        bowlingGame.roll(3);
+        bowlingGame.roll(4);
+        rollMany(16, 0);
+        assertEquals(24, bowlingGame.score());
+    }
+
+    @Test
+    public void testPerfectGame() {
+        rollMany(12, 10);
+        assertEquals(300, bowlingGame.score());
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            bowlingGame.roll(pins);
+        }
+    }
+
+    private void rollSpare() {
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+    }
+
+    private void rollStrike() {
+        bowlingGame.roll(10);
+    }
 }
